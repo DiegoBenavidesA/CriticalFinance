4bb879671ea6181c87c97fb8232d1ed3a8b142d5
ign9cio <ig.peralta@proton.me>
2026-08-21 08:35:12 -0400

    feat: remove legacy code

--- api/src/prisma/prisma.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService], // <- Esto es clave para que otros módulos lo usen
-})
-export class PrismaModule {}
