ef34127932f01d9eee89ff234da6b8ddc2d744eb
ign9cio <ig.peralta@proton.me>
2026-08-21 15:38:54 -0400

    refactor: remove unused import from app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppHttpController } from './app.http';
-import { AppService } from './app.service';
 import { BalanceModule } from './balance/balance.module';
 import { McpModule } from './mcp/mcp.module';
 
