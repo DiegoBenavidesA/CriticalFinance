b4c77c9672f3aea6c68623da7940fcc408bbe5bd
ign9cio <ig.peralta@proton.me>
2026-08-23 05:28:45 -0400

    feat: add environment module with schema, loader, and service

---
+++ apps/api/src/environment/environment.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { EnvironmentService } from "./environment.service";
+
+@Module({
+  providers: [
+    EnvironmentService,
+  ],
+  exports: [
+    EnvironmentService,
+  ],
+})
+export class EnvironmentModule {}
---
+++ apps/api/src/environment/environment.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import { EnvironmentVariables } from "./environment.schema";
+import { loadEnvironmentVariables } from "./environment.loader";
+
+@Injectable()
+export class EnvironmentService implements OnModuleInit {
+  private variables: EnvironmentVariables;
+
+  onModuleInit() {
+    this.variables = loadEnvironmentVariables();
+  }
+
+  get<K extends keyof EnvironmentVariables>(key: K): EnvironmentVariables[K] {
+    return this.variables[key];
+  }
+}
+
