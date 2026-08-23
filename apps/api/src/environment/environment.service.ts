79431e8d8b3bd07e8ff84fdebaecf0752d837453
ign9cio <ig.peralta@proton.me>
2026-08-23 07:32:45 -0400

    refactor: lazy-load environment variables on first get

--- apps/api/src/environment/environment.service.ts
+++ apps/api/src/environment/environment.service.ts
@@ -1,21 +1,23 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { EnvironmentVariables } from "./environment.schema";
 import { loadEnvironmentVariables } from "./environment.loader";
 
 @Injectable()
-export class EnvironmentService implements OnModuleInit {
-  private variables: EnvironmentVariables;
+export class EnvironmentService {
+  private readonly logger = new Logger(EnvironmentService.name);
 
-  onModuleInit() {
-    this.load();
-  }
+  private _variables: EnvironmentVariables;
 
-  load(): void {
-    this.variables = loadEnvironmentVariables();
+  private _load(): void {
+    this._variables = loadEnvironmentVariables();
+    this.logger.log('Environment variables loaded successfully');
   }
 
   get<K extends keyof EnvironmentVariables>(key: K): EnvironmentVariables[K] {
-    return this.variables[key];
+    if (!this._variables)
+      this._load();
+
+    return this._variables[key];
   }
 }
 
