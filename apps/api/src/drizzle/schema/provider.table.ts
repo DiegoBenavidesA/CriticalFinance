6e8f52163ab1099a87723966a7ab67eaf0265482
ign9cio <ig.peralta@proton.me>
2026-09-01 18:54:16 -0400

    feat(api): add provider table

---
+++ apps/api/src/drizzle/schema/provider.table.ts
@@ -0,0 +1,12 @@
+import { pgEnum, pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+
+export const providerIdentifierEnum = pgEnum('provider_identifier', ['fintoc', 'manual']);
+
+export const provider = pgTable('provider', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  identifier: providerIdentifierEnum('identifier').notNull().unique(),
+  createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
+});
+
+export type ProviderRow = typeof provider.$inferSelect;
+export type NewProviderRow = typeof provider.$inferInsert;
