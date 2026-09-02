7ffb6403ab76d0e0af056d65f6c7854458fee6c4
ign9cio <ig.peralta@proton.me>
2026-09-01 20:12:49 -0400

    chore(api): remove unused import in provider table

--- apps/api/src/drizzle/schema/provider.table.ts
+++ apps/api/src/drizzle/schema/provider.table.ts
@@ -1,4 +1,4 @@
-import { pgEnum, pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import { pgEnum, pgTable, timestamp, uuid } from 'drizzle-orm/pg-core';
 
 export const providerIdentifierEnum = pgEnum('provider_identifier', ['fintoc', 'manual']);
 
